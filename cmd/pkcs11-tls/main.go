f2194e02b7f3c0dece0c2ee07653d5547296fabc
Artem Vasilyev <artem.v.vasilyev@gmail.com>
2019-05-31 15:52:54 +0300

    split into smaller functions

--- cmd/pkcs11-tls/main.go
+++ cmd/pkcs11-tls/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto"
 	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
@@ -9,6 +10,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/miekg/pkcs11"
 
@@ -16,27 +18,45 @@ import (
 )
 
 func main() {
-	libPathPtr := flag.String("module", "", "path to the PKCS11 module")
-	pinPtr := flag.String("pin", "", "Smart card PIN")
-	identityURL := flag.String("identity-url", "", "URL of the Identity (Keystone) service (example: https://keystone.stand.loc:443)")
-	caCertPtr := flag.String("cacert", "", "path to the CA certificate (optional)")
+	var (
+		libPath  string
+		pin      string
+		hostname string
+		port     int
+		cacert   string
+		help     bool
+	)
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flag.StringVar(&libPath, "module", "", "path to the PKCS11 module")
+	flag.StringVar(&pin, "pin", "", "Smart card PIN")
+	flag.StringVar(&hostname, "host", "", "Keystone service hostname (example: keystone.stand.loc)")
+	flag.IntVar(&port, "port", 443, "Keystone service port")
+	flag.StringVar(&cacert, "cacert", "", "path to the CA certificate (optional)")
+	flag.BoolVar(&help, "help", false, "show help")
+	var description = `
+Command-line tool for Tokenless Authorization in the Keystone.
+X.509 Client SSL Certificates are stored in the PKCS11 token.
+`
 	flag.Parse()
-	if *libPathPtr == "" {
-		log.Fatal("-module is required")
+	if help == true {
+		fmt.Println(description)
+		flag.Usage()
+		os.Exit(0)
 	}
-	if *pinPtr == "" {
-		log.Fatal("-pin is required")
+	if libPath == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	if *identityURL == "" {
-		log.Fatal("-identity-url is required")
+	if pin == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	slotNumber := 1
-	cfg := &crypto11.Config{
-		Path:       *libPathPtr,
-		SlotNumber: &slotNumber,
-		Pin:        *pinPtr,
+	if hostname == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	context, err := crypto11.Configure(cfg)
+
+	context, err := crypto11Configure(libPath, pin)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,53 +86,53 @@ func main() {
 			continue
 		}
 		x509cert, _ := x509.ParseCertificate(certValue)
-		fmt.Printf("Certificate subject: %s\n", x509cert.Subject)
-		domainName := x509cert.Subject.OrganizationalUnit[0]
-		certificate := tls.Certificate{
-			Certificate: [][]byte{x509cert.Raw},
-			Leaf:        x509cert,
-			PrivateKey:  keyPair,
-		}
 
-		tlsConfig := &tls.Config{
-			Certificates:       []tls.Certificate{certificate},
-			InsecureSkipVerify: true,
-		}
-		if *caCertPtr != "" {
-			caCert, err := ioutil.ReadFile(*caCertPtr)
-			if err != nil {
-				log.Fatal(err)
-			}
-			caCertPool := x509.NewCertPool()
-			caCertPool.AppendCertsFromPEM(caCert)
-			tlsConfig.RootCAs = caCertPool
-		}
-		transport := &http.Transport{TLSClientConfig: tlsConfig}
-		client := &http.Client{Transport: transport}
-
-		identityProvider := getIdentityProvider(x509cert)
-		url := fmt.Sprintf("%s/v3/OS-FEDERATION/identity_providers/%s/protocols/x509/auth", *identityURL, identityProvider)
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Add("X-Domain-Name", domainName)
-		fmt.Printf("requesting token for domain scope '%s': %s\n", domainName, url)
-		resp, err := client.Do(req)
+		token, err := requestToken(x509cert, keyPair, cacert, hostname, port)
 		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		data, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Printf("request failed: %s", err)
+			log.Println(err)
 			continue
 		}
-		fmt.Printf("Response: %s\n", string(data))
-		token := resp.Header.Get("X-Subject-Token")
 		if token != "" {
-			fmt.Printf("Token: %s\n", token)
+			fmt.Printf("token=%s\n", token)
 			break
 		}
 	}
+}
+
+func crypto11Configure(libPath string, pin string) (*crypto11.Context, error) {
+	slotNumber := 1
+	cfg := &crypto11.Config{
+		Path:       libPath,
+		SlotNumber: &slotNumber,
+		Pin:        pin,
+	}
+	context, err := crypto11.Configure(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return context, nil
+}
+
+func requestToken(x509cert *x509.Certificate, keyPair crypto.Signer, cacert string, hostname string, port int) (string, error) {
+	domainName := x509cert.Subject.OrganizationalUnit[0]
+	identityProvider := getIdentityProvider(x509cert)
+	url := fmt.Sprintf(
+		"https://%s:%d/v3/OS-FEDERATION/identity_providers/%s/protocols/x509/auth",
+		hostname, port, identityProvider)
+	client, err := buldTLSCLient(x509cert, keyPair, cacert)
+	if err != nil {
+		return "", err
+	}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("X-Domain-Name", domainName)
+	fmt.Printf("requesting token for domain scope '%s': %s\n", domainName, url)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("request failed: %s", err)
+	}
+	defer resp.Body.Close()
+	token := resp.Header.Get("X-Subject-Token")
+	return token, nil
 }
 
 func getIdentityProvider(cert *x509.Certificate) string {
@@ -125,4 +145,31 @@ func getIdentityProvider(cert *x509.Certificate) string {
 	s.Write([]byte(issuerDN))
 	identityProvider := fmt.Sprintf("%x", s.Sum(nil))
 	return identityProvider
+}
+
+func buldTLSCLient(x509cert *x509.Certificate, keyPair crypto.Signer, cacert string) (*http.Client, error) {
+	certificate := tls.Certificate{
+		Certificate: [][]byte{x509cert.Raw},
+		Leaf:        x509cert,
+		PrivateKey:  keyPair,
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{certificate},
+		InsecureSkipVerify: true,
+	}
+	if cacert != "" {
+		caCertData, err := ioutil.ReadFile(cacert)
+		if err != nil {
+			return nil, err
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCertData)
+		tlsConfig.RootCAs = caCertPool
+	} else {
+		tlsConfig.InsecureSkipVerify = true
+	}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+	return client, nil
 }
